22337174c014973a3729ba03a9e5028349cbfa49
Govind988877Singh <govindsingh988877@gmail.com>
2025-09-03 11:12:04 +0530

    final change

--- models/Member.js
+++ models/Member.js
@@ -1,59 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const memberSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     phoneNo: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     age: {
-//         type: Number,
-//     },
-//     gender: {
-//         type: String,
-//         enum: ["Male","Female","Others"]
-//     },
-//     email: {
-//         type: String,
-//         lowercase: true
-//     },
-//     joiningDate: {
-//         type: Date,
-//         default: Date.now()
-//     },
-//     planDuration: {
-//         type: String,
-//         enum: ["1 month","3 month","6 month","1 year"]
-//     },
-//     feesAmount: {
-//         type: Number,
-//         required: true,
-
-//     },
-//     nextDueDate: {
-//         type: Date,
-//         // required: true
-//     },
-//     paymentStatus: {
-//         type: String,
-//         enum: ["Paid","Pending"],
-//         default: "Pending"
-//     },
-//     lastPaidOn: {
-//         type: Date
-//     },
-//     address: {
-//         type: String,
-//         required: true
-//     },
-
-// },{ timestamps: true })
-
-// module.exports = mongoose.model("Member",memberSchema);
 
 const mongoose = require("mongoose");
 
@@ -72,7 +17,7 @@ const memberSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        enum: ["Male", "Female", "Others"]
+        enum: ["Male","Female","Others"]
     },
     email: {
         type: String,
@@ -84,7 +29,7 @@ const memberSchema = new mongoose.Schema({
     },
     planDuration: {
         type: String,
-        enum: ["1 month", "3 month", "6 month", "1 year"]
+        enum: ["1 month","3 month","6 month","1 year"]
     },
     feesAmount: {
         type: Number,
@@ -96,7 +41,7 @@ const memberSchema = new mongoose.Schema({
     },
     paymentStatus: {
         type: String,
-        enum: ["Paid", "Pending"],
+        enum: ["Paid","Pending"],
         default: "Pending"
     },
     lastPaidOn: {
@@ -106,22 +51,22 @@ const memberSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    
+
     // Additional fields for reminder functionality
     lastReminderSent: {
         type: Date
     },
     paymentMethod: {
         type: String,
-        enum: ["Cash", "Card", "UPI", "Bank Transfer", "Other"],
+        enum: ["Cash","Card","UPI","Bank Transfer","Other"],
         default: "Cash"
     },
     paymentNotes: {
         type: String
     }
-    
-}, { 
-    timestamps: true 
+
+},{
+    timestamps: true
 });
 
 // Add indexes for better query performance
@@ -130,12 +75,13 @@ memberSchema.index({ paymentStatus: 1 });
 memberSchema.index({ phoneNo: 1 });
 
 // Virtual field to get member ID (last 6 characters of _id)
-memberSchema.virtual('memberId').get(function() {
+memberSchema.virtual('memberId').get(function ()
+{
     return this._id.toString().slice(-6);
 });
 
 // Ensure virtual fields are serialized
-memberSchema.set('toJSON', { virtuals: true });
-memberSchema.set('toObject', { virtuals: true });
+memberSchema.set('toJSON',{ virtuals: true });
+memberSchema.set('toObject',{ virtuals: true });
 
-module.exports = mongoose.model("Member", memberSchema);
+module.exports = mongoose.model("Member",memberSchema);
